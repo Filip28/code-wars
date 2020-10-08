83c13bda8e6c53e2f5dc92f8044c5348673ebb1a
Filip <filip.szarek13@gmail.com>
2020-10-08 08:00:31 +0200

    # 1 Add exercise where you have to replace chars in string: * 'A' <-> 'T' * 'C' <-> 'G'

---
+++ src/main/java/codebyte/DnaStrand.java
@@ -0,0 +1,29 @@
+package codebyte;
+
+public class DnaStrand {
+
+    public static void main(String[] args) {
+        System.out.println(makeComplement("ATTGC"));
+    }
+
+    public static String makeComplement(String dna) {
+        char[] chars = dna.toCharArray();
+        return replaceChars(chars);
+    }
+
+    private static String replaceChars(char[] chars) {
+        StringBuilder sb = new StringBuilder();
+        for (char ch : chars) {
+            if (ch == 'A') {
+                sb.append('T');
+            } else if (ch == 'T') {
+                sb.append('A');
+            } else if (ch == 'C') {
+                sb.append('G');
+            } else {
+                sb.append('C');
+            }
+        }
+        return sb.toString();
+    }
+}
