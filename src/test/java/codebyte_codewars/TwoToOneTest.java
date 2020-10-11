19635e1b1ccbe7fa4657982af1480e294d4996ba
Filip <filip.szarek13@gmail.com>
2020-10-11 16:51:44 +0200

    # 4 Add class with static method which merge two string, remove duplicates and sort it. After that return result String. Add test method.

---
+++ src/test/java/codebyte_codewars/TwoToOneTest.java
@@ -0,0 +1,23 @@
+package codebyte_codewars;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoToOneTest {
+
+    @Test
+    void shouldReturnSortedDistinctString() {
+        // given
+        String s1 = "aretheyhere";
+        String s2 = "yestheyarehere";
+
+        // when
+        String result = TwoToOne.longest(s1, s2);
+
+        // then
+        assertEquals("aehrsty", result);
+
+    }
+
+}
