cd088b5b06d4f1b54c749f04661ca15b8b320f4c
Filip <filip.szarek13@gmail.com>
2020-10-21 07:04:22 +0200

    # 12 Create method to get middle character/characters from given string.

---
+++ src/test/java/codebyte_codewars/Kata1Test.java
@@ -0,0 +1,47 @@
+package codebyte_codewars;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.of;
+
+class Kata1Test {
+
+    Kata1 kata = new Kata1();
+
+    @ParameterizedTest
+    @MethodSource("providesStringForTest")
+    void shouldReturnOneLetterFromTheMiddleOfString(String word, String expectedResult) {
+        // given + when
+        String result = kata.getMiddle(word);
+
+        // then
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    void shouldReturnTwoLettersFromTheMiddleOfString() {
+        // given
+        String s = "test";
+
+        // when
+        String result = kata.getMiddle(s);
+
+        // then
+        assertEquals("es", result);
+    }
+
+    private static Stream<Arguments> providesStringForTest() {
+        return Stream.of(
+                of("A", "A"),
+                of("testing", "t")
+        );
+    }
+}
