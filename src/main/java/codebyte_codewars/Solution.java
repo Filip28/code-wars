cabd123f7f3bad8cabf3b8828a6d5bcbace27b02
Filip <filip.szarek13@gmail.com>
2020-10-25 08:27:30 +0100

    # 16 Create method which change given string with "-" or "_" on CamelCase String

---
+++ src/main/java/codebyte_codewars/Solution.java
@@ -0,0 +1,14 @@
+package codebyte_codewars;
+
+import java.util.Arrays;
+
+class Solution {
+
+    public String toCamelCase(String s) {
+        String[] words = s.split("[-_]+");
+        return Arrays.stream(words)
+                .skip(1)
+                .map(w -> w.substring(0, 1).toUpperCase().concat(w.substring(1)))
+                .reduce(words[0], String::concat);
+    }
+}
