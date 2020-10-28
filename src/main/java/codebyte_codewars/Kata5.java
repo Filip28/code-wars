f301b8bb42baaa970d628aa07c15a2f805d99f4e
Filip <filip.szarek13@gmail.com>
2020-10-28 07:04:54 +0100

    # 19 Create method to flatten 2D array into 1D

---
+++ src/main/java/codebyte_codewars/Kata5.java
@@ -0,0 +1,14 @@
+package codebyte_codewars;
+
+import java.util.Arrays;
+
+public class Kata5 {
+
+    public int[] flattenAndSort(int[][] array) {
+        return Arrays.stream(array)
+                .flatMapToInt(Arrays::stream)
+                .sorted()
+                .toArray();
+    }
+
+}
