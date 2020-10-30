e3ae803d9f06c4ef15758d846f49a41426a9482e
Filip <filip.szarek13@gmail.com>
2020-10-30 07:06:34 +0100

    Create method to calculate after how many years we get desired sum of money in bank

---
+++ src/main/java/codebyte_codewars/Money.java
@@ -0,0 +1,12 @@
+package codebyte_codewars;
+
+public class Money {
+    public int calculateYears(double principal, double interest,  double tax, double desired) {
+        int years = 0;
+        while (principal < desired) {
+            principal += principal * interest * (1 - tax);
+            years++;
+        }
+        return years;
+    }
+}
